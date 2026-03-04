28e388a24edaf3452d8fb3f677902f6b450c3a0e
vidhyasagar <vs1522250@gmail.com>
2026-03-04 13:00:44 +0530

    terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./app.scss"
 import Dock from './components/Dock.jsx'
 import Nav from "./components/Nav.jsx"
+import MacWindow from "./components/windows/MacWindow.jsx"
 function App() {
 
   return (
     <main>
       <Nav />
       <Dock />
+      <MacWindow >
+        hello
+      </MacWindow>
     </main>
   )
 }
